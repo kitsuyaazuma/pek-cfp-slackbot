0012fb2c966232d3921c61a564a36b7d12f458fe
kitsuyaazuma <kitsuyaazuma@gmail.com>
2025-06-19 01:18:11 +0900

    feat: add cron-based status reminder
    Signed-off-by: kitsuyaazuma <kitsuyaazuma@gmail.com>

--- src/utils/slack.ts
+++ src/utils/slack.ts
@@ -1,19 +1,23 @@
 export const postSlackMessage = async (
   token: string,
   channel: string,
-  thread_ts: string,
   text: string,
-) => {
-  await fetch("https://slack.com/api/chat.postMessage", {
+  thread_ts?: string,
+): Promise<Response> => {
+  const body: Record<string, unknown> = {
+    channel: channel,
+    text: text,
+  };
+  if (thread_ts) {
+    body.thread_ts = thread_ts;
+  }
+  const res = await fetch("https://slack.com/api/chat.postMessage", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify({
-      channel: channel,
-      thread_ts: thread_ts,
-      text: text,
-    }),
+    body: JSON.stringify(body),
   });
+  return res;
 };
